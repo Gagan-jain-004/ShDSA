1e94d449f5a46a7c5360a85681cc0416998ce068
Gagan jain004 <gagan.praveen004@gmail.com>
2025-07-31 13:53:22 +0530

    stl shot

---
+++ d-30 Greedy/MinAbsoluteDiff.cpp
@@ -0,0 +1,18 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// approach sort the both array and then pair them index wise to get min diff.
+
+int main(){
+
+    vector<int>A = {4,1,8,7};
+    vector<int>B= {2,3,6,5};
+
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+
+   vector<pair<int,int>>
+    return 0;
+}
