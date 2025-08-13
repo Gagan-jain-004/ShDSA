ff5f6598aa414859c9bc94f5d722310b658515b0
Gagan jain004 <gagan.praveen004@gmail.com>
2025-08-13 19:10:53 +0530

    #LCA stuff

---
+++ d-33 BT Ancestor/KthLevelofTree.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+    public:
+        int data;
+        Node*left;
+        Node*right;
+
+        Node(int data){
+            this->data = data;
+            left=right=NULL;
+        }
+
+};
+
+// Tree building
+
+static int idx= -1;
+
+Node* buildTree(vector<int>nodes){              // it build tree and this takes nodes vectore as input  and return the address of root node as return type is Node*
+    
+    idx++;                           //as it is static so it will ++ wherever we go
+    
+    if(nodes[idx] == -1)  return  NULL;
+
+     Node* currNode= new Node(nodes[idx]);    
+    currNode->left = buildTree(nodes);               // current ke left sai  point krwado ,  note joining  strts with  returning null
+    currNode->right= buildTree(nodes);
+
+    return  currNode;
+
+}
+
+void KthHelper(Node* root,int k,int currLevel){
+    if(root ==NULL){
+        return;
+    }
+
+    if(currLevel==k){
+        cout<<root->data<<" ";
+        return;
+    }
+
+    KthHelper(root->left,k,currLevel+1);  //left
+    KthHelper(root->right,k,currLevel+1);  //right
+}
+
+void KthLevel(Node* root,int k){
+    KthHelper(root,k,1);
+    cout<<endl;
+}
+
+
+
+int main() {
+
+    vector<int> nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+    Node*root = buildTree(nodes);                            // O(n)
+
+    KthLevel(root,3);
+  
+
+    return 0;
+}
+
+
+ 
