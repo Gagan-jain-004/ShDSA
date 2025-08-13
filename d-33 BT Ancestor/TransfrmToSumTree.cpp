95c3a1a1192d0236b2ec4aadf34a363bfffeb2b1
Gagan jain004 <gagan.praveen004@gmail.com>
2025-08-13 23:06:11 +0530

    Trsfrm2sum

---
+++ d-33 BT Ancestor/TransfrmToSumTree.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+    public:
+        int data;
+        Node*left;
+        Node*right;
+
+        Node(int data){
+            this->data = data;
+            left=right=NULL;
+        }
+
+};
+
+// Tree building
+
+static int idx= -1;
+
+Node* buildTree(vector<int>nodes){              // it build tree and this takes nodes vectore as input  and return the address of root node as return type is Node*
+    
+    idx++;                           //as it is static so it will ++ wherever we go
+    
+    if(nodes[idx] == -1)  return  NULL;
+
+     Node* currNode= new Node(nodes[idx]);    
+    currNode->left = buildTree(nodes);               // current ke left sai  point krwado ,  note joining  strts with  returning null
+    currNode->right= buildTree(nodes);
+
+    return  currNode;
+
+}
+
+int transform(Node*root){
+
+    if(root ==NULL) return 0;
+
+    int leftOld = transform(root->left);
+    int rightOld= transform(root->right);
+    int currOld = root->data;
+
+    root->data = leftOld +rightOld+root->left->data + root->right->data;
+    
+    if(root->left !=NULL) root->data += root->left->data;
+    
+    if(root->right !=NULL) root->data += root->right->data;
+
+
+    return currOld;
+
+}
+
+
+int main() {
+
+    vector<int> nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+    Node*root = buildTree(nodes);                            // O(n)
+    
+    transform(root);
+
+    return 0;
+}
+
+
+ 
