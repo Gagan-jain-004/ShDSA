8dd456be5fdfe99e1e3407a06b791ecddc890814
Gagan jain004 <gagan.praveen004@gmail.com>
2025-08-18 17:00:09 +0530

    #changes

--- d-36 Heap1/HeapViaVector.cpp
+++ d-36 Heap1/HeapViaVector.cpp
@@ -25,3 +25,91 @@ let say child idx = x       then parent idx = x/2
 */
 
 
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Heap{
+  vector<int>vec; 
+public:
+
+void push(int val){    //O(logn)
+
+  // step 1- insert the val in end 
+  vec.push_back(val);
+
+  // step-2 fix heap  
+  int x = vec.size()-1;                 // accesing the index of new val added (childI)
+  int parI = (x-1)/2;              // accessing parent of new val(childI)
+
+  while(parI >=0 && vec[x]>vec[parI] ){
+    swap(vec[x],vec[parI]);
+    x= parI;
+    parI= (x-1)/2;
+
+  }
+
+}
+
+void heapify(int i){
+
+  if(i>=vec.size())   return;
+
+  int l = 2*i+1;
+  int r = 2*i +2;
+
+  int maxIdx = i;
+  if(l<vec.size() && vec[l]>vec[maxIdx]){
+    maxIdx =l;
+  }
+
+  if(r<vec.size() && vec[r]>vec[maxIdx]){
+    maxIdx =l;
+  }
+
+  swap(vec[i],vec[maxIdx]);
+  if(maxIdx != i){
+    heapify(maxIdx);
+  }
+
+}
+
+void pop(){
+// step1 - swap with end index
+  swap(vec[0],vec[vec.size()-1]);
+
+  // step2 - now delete the last 
+  vec.pop_back();
+
+  // step3 - heapify
+  heapify(0); 
+
+}
+
+int top(){
+  return vec[0];   // highest priority element 
+}
+
+bool empty(){
+  return vec.size()==0;
+}
+
+
+};
+
+
+int main() {
+
+Heap heap;
+heap.push(50);
+heap.push(10);
+heap.push(100);
+
+cout<<"top = "<<heap.top()<<endl;
+heap.pop();
+cout<<"top = "<<heap.top()<<endl;
+
+    return 0;
+}
