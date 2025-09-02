ec8a3241e5baa5e932f5784773b3e0925a6e381e
Gagan jain004 <gagan.praveen004@gmail.com>
2025-09-02 13:15:23 +0530

    knapsack01 tabulation

--- d-47 DPknapsack/01KnapByTabulation.cpp
+++ d-47 DPknapsack/01KnapByTabulation.cpp
@@ -63,33 +63,7 @@ int knapsackMem(vector<int>val,vector<int>wt,int W,int n,vector<vector<int>>&dp)
 
 }
 
-int knapsackTab(vector<int>val,vector<int>wt,int W,int n){
-    vector<vector<int>>dp(n+1,vector<int>(W+1,0));
-
-    for(int i=1;i<n+1;i++){
-        for(int j=1;j<W+1;j++){
-            int itemWt= wt[i-1];
-            int itemVal = val[i-1];
-
-            if(itemWt <=j){
-                dp[i][j] = max(itemVal +dp[i-1][j-itemWt],dp[i-1][j]);
-            }
-            else{
-                dp[i][j]= dp[i-1][j];
-            }
-
-
-        }
-    }
-    cout<<"tabulation array "<<endl;
-    for(int i=0;i<n+1;i++){
-        for(int j=0;j<W+1;j++){
-            cout<<dp[i][j]<<" ";
-        }cout<<endl;
-    }
-
-    return dp[n][W];
-}
+// dp tabulation approach             (recursion base case are initialization of tabulation)
 
 int main() {
 
@@ -98,7 +72,18 @@ int main() {
     int W =7;
     int n= 5;
 
-    cout<<knapsackTab(val,wt,W,n)<<endl;
- 
+    cout<<knapsackRec(val,wt,W,n)<<endl;
+
+    cout<<"dp memo"<<endl;
+
+    vector<vector<int>>dp(n+1,vector<int>(W+1,-1));
+    cout<<knapsackMem(val,wt,W,n,dp)<<endl;
+
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<W+1;j++){
+            cout<<dp[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
