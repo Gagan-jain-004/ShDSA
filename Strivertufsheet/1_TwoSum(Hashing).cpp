bedbd758faf95d704e7b47821a368d94c2b67e93
Gagan jain004 <gagan.praveen004@gmail.com>
2025-10-03 22:15:29 +0530

    #soln

--- Strivertufsheet/1_TwoSum(Hashing).cpp
+++ Strivertufsheet/1_TwoSum(Hashing).cpp
@@ -9,7 +9,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       unordered_map<int,int>m;             //value and index
+       unordered_map<int,int>m;             //value and index mapping
     
        for(int i=0;i<nums.size();i++){
             int rem = target - nums[i];
